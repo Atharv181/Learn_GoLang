774a18a53abbfc68689a54fb6ca051af9d999ed1
Atharv181 <atharvbobade181@gmail.com>
2023-02-22 18:59:49 +0530

    Array folder added

---
+++ 07Array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Array in GO")
+	// Not used much in language instead slices used
+
+	var names [3]string
+
+	names[0] = "Atharv"
+	names[1] = "King"
+
+	fmt.Println("Value of array is",names)
+	//OP 
+	//Value of array is [Atharv King ]
+
+	fmt.Println("Lenght of array is",len(names))
+	//Lenght of array is 3
+
+	var surname = [4]string{"Bobade" , "Hello" , "Queen", "OK"}
+	fmt.Println(surname)
+	fmt.Println("Lenght of array is",len(surname))
+	//[Bobade Hello Queen OK]
+	//Lenght of array is 4
+
+}
